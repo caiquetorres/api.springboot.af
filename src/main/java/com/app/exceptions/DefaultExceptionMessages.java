fe2ab792ccdc917f1d4ec1c89660ffce24259ef0
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-11-20 18:58:19 -0300

    feat(DefaultExceptionMessages.java): add a simple quote before the parameter

--- src/main/java/com/app/exceptions/DefaultExceptionMessages.java
+++ src/main/java/com/app/exceptions/DefaultExceptionMessages.java
@@ -2,6 +2,6 @@ package com.app.exceptions;
 
 public final class DefaultExceptionMessages {
     public static final String entityNotFound(String identifier) {
-        return "The entity identified by " + identifier + " was not found";
+        return "The entity identified by " + '\'' + identifier + '\'' + " was not found";
     }
 }
